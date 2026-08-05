43efd039a74ad5ded4c9243574952ad174ac39ce
Shampad Sharkar <shampad@anymindgroup.com>
2026-08-05 23:04:29 +0600

    update environment variables and improve run script structure

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import os
 import uvicorn
 
-uvicorn.run("app.main:app", host="0.0.0.0", port=int(os.getenv("PORT", "8000")), reload=True)
+if __name__ == "__main__":
+    uvicorn.run("app.main:app", host="0.0.0.0", port=int(os.getenv("PORT", "8000")), reload=True)
